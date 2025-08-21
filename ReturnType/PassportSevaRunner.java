5529acfd5d7802e690202ae7ec23dcc85a490677
SannidhiGS <ssannidhigs@gmail.com>
2025-08-21 20:47:38 +0530

    creating class in java

--- ReturnType/PassportSevaRunner.java
+++
@@ -1,9 +0,0 @@
-class PassportSevaRunner{
-	public static void main(String args[]){
-		boolean isUserRegistered=PassportSeva.isPass("Sannidhi","Shetty","Sannidhi@129","Sannidhi@123");
-		System.out.println("Is user registered "+isUserRegistered);
-		
-		if(isUserRegistered)
-		PassportSeva.userDetails();
-	}
-}
