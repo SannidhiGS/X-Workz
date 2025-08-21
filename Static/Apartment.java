623485a5d4a09ffa6560c93045557ee118a03d52
SannidhiGS <ssannidhigs@gmail.com>
2025-08-21 20:34:14 +0530

    adding static variables

---
+++ Static/Apartment.java
@@ -0,0 +1,45 @@
+class Apartment {
+    static String apartmentName = "Shivashakti";
+    static String location = "JP Nagar 7th phase, Bangalore";
+    static String builderName = "ABC Constructions";
+    static int totalFloors = 10;
+    static int totalUnits = 100;
+    static int unitsOccupied = 90;
+    static int unitsAvailable = 10;
+    static double averageRentPerUnit = 15000.0;
+    static boolean hasSwimmingPool = true;
+    static boolean hasGym = true;
+    static boolean hasClubHouse = true;
+    static boolean hasChildrenPlayArea = true;
+    static boolean hasParkingFacility = true;
+    static boolean hasSecurity = true;
+    static boolean hasElevators = true;
+    static int noOfSecurityGuards = 5;
+    static int noOfStaff = 10;
+    static String maintenanceContact = "080-12345678";
+    static String apartmentManager = "Mr. Ramesh Kumar";
+    static int yearBuilt = 2018;
+
+    public static void main(String[] args) {
+        System.out.println("Apartment Name: " + apartmentName);
+        System.out.println("Location: " + location);
+        System.out.println("Builder: " + builderName);
+        System.out.println("Total Floors: " + totalFloors);
+        System.out.println("Total Units: " + totalUnits);
+        System.out.println("Units Occupied: " + unitsOccupied);
+        System.out.println("Units Available: " + unitsAvailable);
+        System.out.println("Average Rent per Unit: ₹" + averageRentPerUnit);
+        System.out.println("Has Swimming Pool: " + hasSwimmingPool);
+        System.out.println("Has Gym: " + hasGym);
+        System.out.println("Has Club House: " + hasClubHouse);
+        System.out.println("Has Children Play Area: " + hasChildrenPlayArea);
+        System.out.println("Has Parking Facility: " + hasParkingFacility);
+        System.out.println("Has Security: " + hasSecurity);
+        System.out.println("Has Elevators: " + hasElevators);
+        System.out.println("Number of Security Guards: " + noOfSecurityGuards);
+        System.out.println("Number of Staff: " + noOfStaff);
+        System.out.println("Maintenance Contact: " + maintenanceContact);
+        System.out.println("Apartment Manager: " + apartmentManager);
+        System.out.println("Year Built: " + yearBuilt);
+    }
+}
